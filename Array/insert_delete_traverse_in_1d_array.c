40e833403640047b4ceccdd5ba98fc146228d7b9
shantanu904 <shantanuchauhan562@gmail.com>
2026-09-13 19:14:27 +0530

    Add files via upload

---
+++ Array/insert_delete_traverse_in_1d_array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int a[100], n, i;
+
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    printf("Enter elements: ");
+    for(i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    printf("Array elements are: ");
+    for(i = 0; i < n; i++)
+        printf("%d ", a[i]);
+
+    return 0;
+}
